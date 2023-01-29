2171ccb96baa3402ed10f452f4848ac0fa3fed5f
GDiana <dcquinche@gmail.com>
2023-01-29 16:08:04 -0500

    Add NavBar Component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+import './styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle, faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <section className="navbar__pages">
+        <p className="navbar__items">Inicio</p>
+        <Dropdown>
+          <Dropdown.Toggle className="navbar__items navbar__categories" variant="success" id="dropdown-basic">Categorías</Dropdown.Toggle>
+          <Dropdown.Menu className="navbar__categoriesMenu">
+            <Dropdown.Item className="navbar__categoriesItems">Atrapasueños</Dropdown.Item>
+            <Dropdown.Item className="navbar__categoriesItems">Cojines</Dropdown.Item>
+            <Dropdown.Item className="navbar__categoriesItems">Collares</Dropdown.Item>
+            <Dropdown.Item className="navbar__categoriesItems">Portamacetas</Dropdown.Item>
+            <Dropdown.Item className="navbar__categoriesItems">Tapices</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </section>
+      <section className="navbar__icons">
+        <FontAwesomeIcon className="navbar__items" icon={faUserCircle} />
+        <FontAwesomeIcon className="navbar__items" icon={faCartShopping} />
+      </section>
+    </nav>
+
+  )
+}
+
+export default NavBar;

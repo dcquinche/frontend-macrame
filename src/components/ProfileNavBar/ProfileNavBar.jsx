a000ddf2adb52c6f05bdb7b16086a630e1fcd5a8
GDiana <dcquinche@gmail.com>
2023-02-05 08:59:48 -0500

    Add funcionality to the ProfileNavBar

--- src/components/ProfileNavBar/ProfileNavBar.jsx
+++ src/components/ProfileNavBar/ProfileNavBar.jsx
@@ -2,12 +2,22 @@ import './styles.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faUserEdit, faBagShopping } from '@fortawesome/free-solid-svg-icons';
 
-const ProfileNavBar = () => {
+const ProfileNavBar = ({ setNavBar }) => {
+  const handleClickProfile = () => {
+    setNavBar('Profile')
+  }
+  const handleClickEditProfile = () => {
+    setNavBar('EditProfile')
+  }
+  const handleClickShoppingBag = () => {
+    setNavBar('ShoppingBag')
+  }
+
   return (
     <div className='ProfileNavBar'>
-      <button className='ProfileNavBar__button'><FontAwesomeIcon className='ProfileNavBar__icon' icon={faUser} /> Perfil</button>
-      <button className='ProfileNavBar__button'><FontAwesomeIcon className='ProfileNavBar__icon' icon={faUserEdit} /> Editar Perfil</button>
-      <button className='ProfileNavBar__button'><FontAwesomeIcon className='ProfileNavBar__icon' icon={faBagShopping} /> Compras</button>
+      <button className='ProfileNavBar__button' onClick={handleClickProfile}><FontAwesomeIcon className='ProfileNavBar__icon' icon={faUser} /> Perfil</button>
+      <button className='ProfileNavBar__button' onClick={handleClickEditProfile}><FontAwesomeIcon className='ProfileNavBar__icon' icon={faUserEdit} /> Editar Perfil</button>
+      <button className='ProfileNavBar__button' onClick={handleClickShoppingBag}><FontAwesomeIcon className='ProfileNavBar__icon' icon={faBagShopping} /> Compras</button>
     </div>
   )
 }
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,38 @@
 import './styles.css';
+import { useState } from 'react';
 import ProfileNavBar from '../../components/ProfileNavBar/ProfileNavBar';
+import UserInfo from '../../components/UserInfo/UserInfo';
+import UserInfoForm from '../../components/UserInfoForm/UserInfoForm';
+import UserPictureForm from '../../components/UserPictureForm/UserPictureForm';
 
 const Profile = () => {
+  const [navBar, setNavBar] = useState('Profile');
+
   return (
     <div className='profilePage'>
-      <ProfileNavBar />
+      <ProfileNavBar navBar={navBar} setNavBar={setNavBar} />
+      {
+        navBar === 'Profile' ? (
+          <section>
+            <UserInfo />
+          </section>
+        ) : null
+      }
+      {
+        navBar === 'EditProfile' ? (
+          <section className='profilePage__EditProfileTab'>
+            <UserPictureForm />
+            <UserInfoForm />
+          </section>
+        ) : null
+      }
+      {
+        navBar === 'ShoppingBag' ? (
+          <section>
+            Soy Shopping Bag
+          </section>
+        ) : null
+      }
     </div>
   )
 }

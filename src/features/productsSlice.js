a052c1dd0cef2d4519ab69ae7a15a0adeaebc462
GDiana <dcquinche@gmail.com>
2023-02-08 08:55:24 -0500

    test vercel variable environment

--- src/features/productsSlice.js
+++ src/features/productsSlice.js
@@ -1,13 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const BASE_URL = 'https://backend-macrame-production.up.railway.app';
-
 const initialState = {
   products: [],
 }
 
 export const getAllProducts = createAsyncThunk('products/getAllProducts', async () => {
-  const response = await fetch(`${BASE_URL}/api/products`);
+  const response = await fetch(`${REACT_APP_BASE_URL}/api/products`);
   const data = await response.json();
   return data;
 });
